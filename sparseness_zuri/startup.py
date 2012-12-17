6b22e75c05c089c60d84594772afc3e341849cd9
chausler <chausler@gmail.com>
2012-12-17 10:04:05 +0100

    path for bommel

--- sparseness_zuri/startup.py
+++ sparseness_zuri/startup.py
@@ -1,5 +1,11 @@
+#
+#print 'X220'
+#data_path = '/home/chris/workspace/phd/data/'
+#fig_path = '/home/chris/workspace/phd/fig/'
+#extern_data_path = '/media/miniElements/data/'
 
-print 'X220'
-data_path = '/home/chris/workspace/phd/data/'
-fig_path = '/home/chris/workspace/phd/fig/'
-extern_data_path = '/media/miniElements/data/'
+
+print 'bommel'
+data_path = '/home/chris/code/phd/trunk/data/'
+fig_path = '/home/chris/code/phd/trunk/fig/'
+extern_data_path = '/home/chris/data/'
